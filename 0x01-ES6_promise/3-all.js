7deb9b715676dfd7fb6e13b8956ab384c5724de8
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-04-24 22:08:40 +0100

    update

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const p1 = uploadPhoto();
-  const p2 = createUser();
+  const img = uploadPhoto();
+  const usr = createUser();
 
-  return Promise.all([p1, p2]).then((value) => {
+  return Promise.all([img, usr]).then((value) => {
     console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
   })
     .catch(() => { console.log('Signup system offline'); });
