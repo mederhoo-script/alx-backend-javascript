5bc3bc98054b2c26202bde9b6cbc47fae0c6b86a
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-04-24 22:00:04 +0100

    promise updates

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+export default function getFullResponseFromAPI(success) {
+  const array = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+  return array;
+}
